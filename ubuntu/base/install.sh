b35060aa67396f4e43ca6cf96656fb13c30cee04
kindtek <n8@kindtek.io>
2022-11-30 04:47:04 -0800

    remove systemctl stuff

--- ubuntu/base/install.sh
+++ ubuntu/base/install.sh
@@ -49,6 +49,3 @@ fi; \
 \
 curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose; \
 chmod +x /usr/local/bin/docker-compose; \
-systemctl enable docker.service \
-systemctl daemon-reload \
-systemctl start docker.service \
