3d122cd0b0e0a2d99281d6399fcdc65201ec4165
kindtek <n8@kindtek.io>
2022-12-01 12:44:46 -0800

    copy the whole contents of known working folder

--- ubuntu/base/fix-malformed-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# shorten the list by two
-chmod +x /etc/apt/sources.list
-sed '52,53d' /etc/apt/sources.list
-echo "\n ######### /etc/apt/sources.list shortened by two lines ###########\n"
-# only needs to be called once
-/bin/rm -f "${0}"
