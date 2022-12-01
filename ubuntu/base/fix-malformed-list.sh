5a9bd5643b1aadb8486b4b081ade5a155ecb1a75
kindtek <n8@kindtek.io>
2022-12-01 02:20:51 -0800

    teracy dockerfile tweaks

--- ubuntu/base/fix-malformed-list.sh
+++ ubuntu/base/fix-malformed-list.sh
@@ -3,6 +3,6 @@
 # shorten the list by two
 chmod +x /etc/apt/sources.list
 sed '52,53d' /etc/apt/sources.list
-echo "\n/etc/apt/sources.list shortened by two lines\n"
+echo "\n ######### /etc/apt/sources.list shortened by two lines ###########\n"
 # only needs to be called once
 /bin/rm -f "${0}"
